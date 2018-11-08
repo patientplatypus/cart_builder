cad4d4768a9b44b96b78f801ffa39c4d19c4df80
JessicaSung <18690072+JessicaSung@users.noreply.github.com>
2018-11-07 22:15:20 -0600

    Completed header and footer

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -17,45 +17,29 @@ export default class Example extends React.Component {
   state = {
     isOpen: false
   }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
+  toggle = () => {
+    this.setState(() => ({ isOpen: !this.state.isOpen }));
   }
   render() {
     return (
       <div>
         <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarBrand href="#">Your Store</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/components/">Components</NavLink>
+                <NavLink href="#">All Departments</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                <NavLink href="#">My Account</NavLink>
+              </NavItem><NavItem>
+                <NavLink href="#">Cart</NavLink>
               </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
             </Nav>
           </Collapse>
         </Navbar>
+        <div className="header-tail"></div>
       </div>
     );
   }
